1a7c5257e5ec1814469d99b3a0ce22c5c0e03f90
Kulikov Valery <valenok801@gmail.com>
2023-01-30 15:08:31 +0300

    Initialise project

---
+++ Shooter/Assets/_Source/Core/Bootstrapper.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+namespace _Source.Core
+{
+    public class Bootstrapper : MonoBehaviour
+    {
+        private void Awake()
+        {
+            
+        }
+    }
+}
